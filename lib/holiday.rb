f40d9fc6790b3edf00d61fc2fe7f4a0ceeefd4c4
Robert Snyder <r.snyder49@gmail.com>
2018-08-20 20:20:27 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -67,8 +67,9 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
   holiday_hash.each do |season, holiday| 
     puts "#{season.to_s.capitalize!}:"
-    
-   
+    holiday.each do |holiday, supplies|
+      puts holiday
+    end
   end
 end 
 

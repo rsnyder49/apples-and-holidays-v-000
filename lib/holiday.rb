7201fed927b2c4ca67f34a9dc3eb09f7c2da6ddd
Robert Snyder <r.snyder49@gmail.com>
2018-08-20 20:35:04 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -76,7 +76,7 @@ def all_supplies_in_holidays(holiday_hash)
         cap_supply = supply.capitalize! 
       end
       puts "#{holiday_array.join(" ")}: #{cap_supply(", ")}"
-      end
+      
     end
   end
 end 

207a1af728e1699fc5ab1720ad5166872e6030fb
Robert Snyder <r.snyder49@gmail.com>
2018-08-20 20:22:10 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -68,7 +68,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday| 
     puts "#{season.to_s.capitalize!}:"
     holiday.each do |holiday, supplies|
-      holiday_array = holiday.split("_")
+      holiday_array = holiday.to_s.split("_")
       holiday_array.each do |word|
         puts word
       end
